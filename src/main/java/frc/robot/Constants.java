eda372f76674bc56ef5f86a2993cd4acfd60db66
Kyle Burman <geminifrc4575@gmail.com>
2026-06-30 10:25:29 -0400

    Also test commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,7 +9,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
-
 /**
  * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
  * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
